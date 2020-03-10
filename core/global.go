ae4785b7747dcbcf8f80fa766ac1b2c0f2f88f37
shitingbao <871258317@qq.com>
2020-03-10 17:53:01 +0800

    增加日志模块，操作起始init待定

--- core/global.go
+++ core/global.go
@@ -3,10 +3,13 @@ package core
 import (
 	"linux_test_golang/lib/config"
 	"sync"
+
+	"github.com/shitingbao/datelogger"
 )
 
 var (
 	Db            string //数据库连接
 	WebConfig     config.Config
 	TaskWaitGroup = new(sync.WaitGroup)
+	LOG           *datelogger.DateLogger
 )
