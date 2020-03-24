6ecbefa1f80a686c72248090a2a93ca2f8dd2c19
shitingbao <871258317@qq.com>
2020-03-24 09:26:22 +0800

    简化

--- modules/common/images_toword.go
+++ modules/common/images_toword.go
@@ -11,54 +11,61 @@ import (
 //ImageWord 业务类
 type ImageWord struct{}
 
-type image struct {
-	Image []string
-}
-
 func init() {
 	core.RegisterFun("image", new(ImageWord))
 }
 
 //Post 图片转文字
 func (im *ImageWord) Post(p *core.ElementHandleArgs) {
+	imageURLs, err := getFileHands(p)
+	if err != nil {
+		core.SendJSON(p.Res, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	result, err := imagesOpera(imageURLs, p)
+	if err != nil {
+		core.SendJSON(p.Res, http.StatusOK, err.Error())
+		return
+	}
+
+	core.SendJSON(p.Res, http.StatusOK, result)
+}
+
+//获取表单内图片保存，并反馈对应所有图片路径
+func getFileHands(p *core.ElementHandleArgs) ([]string, error) {
 	imageURLs := []string{}
 	fileHands := formopera.GetAllFormFiles(p.Req)
 	for _, v := range fileHands {
 		file, err := v.Open()
 		if err != nil {
-			core.SendJSON(p.Res, http.StatusInternalServerError, err.Error())
-			return
+			return imageURLs, err
 		}
 		imageURL, err := images.ByteToImage(file)
 		if err != nil {
-			core.SendJSON(p.Res, http.StatusInternalServerError, err.Error())
-			return
+			return imageURLs, err
 		}
 		imageURLs = append(imageURLs, imageURL)
 		file.Close()
 	}
+	return imageURLs, nil
+}
+
+//imagesOpera 传入图片路径，亲求三方接口反馈文字对象
+func imagesOpera(imageURLs []string, p *core.ElementHandleArgs) ([]imagetowordapi.AcceptResultWord, error) {
 	result := []imagetowordapi.AcceptResultWord{}
 	for _, v := range imageURLs {
 		imagesBase64 := []string{}
 		base64, err := images.ImageToBase64(v)
 		if err != nil {
-			core.SendJSON(p.Res, http.StatusOK, core.SendMap{"err": err.Error()})
-			return
+			return result, err
 		}
 		imagesBase64 = append(imagesBase64, base64)
 		res, err := imagetowordapi.GetImageWord(imagesBase64)
 		if err != nil {
-			core.SendJSON(p.Res, http.StatusOK, err.Error())
-			return
+			return result, err
 		}
 		result = append(result, res)
 	}
-
-	core.SendJSON(p.Res, http.StatusOK, core.SendMap{"data": result})
-}
-
-func imagesOpera(pa interface{}, p *core.ElementHandleArgs) error {
-	param := pa.(*image)
-	core.SendJSON(p.Res, http.StatusOK, param)
-	return nil
+	return result, nil
 }
