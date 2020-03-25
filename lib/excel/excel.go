8d8609fb4cceeb1c77ad9798eda503ce13d861ad
shitingbao <871258317@qq.com>
2020-03-25 17:15:16 +0800

    excel外部函数添加

--- lib/excel/excel.go
+++ lib/excel/excel.go
@@ -10,17 +10,15 @@ import (
 	"github.com/tealeg/xlsx"
 )
 
-//Excel 数据对象
-type Excel struct {
-	FileName   string //文件名称，不需要文件后缀
-	SheetNum   int    //sheet的数量,与sheet的数据数组长度对应
-	SheetDatas []SheetData
+//excel 数据对象
+type excel struct {
+	FileName   string      //文件名称，不需要文件后缀
+	SheetDatas []sheetData //sheet的数量,与sheet的数据数组长度对应
 }
 
-//SheetData sheet数据内容
-type SheetData struct {
-	Title []string            //标题列
-	Rows  []map[string]string //行，每一行的数据以标题为标准key，存储
+//sheetData sheet数据内容
+type sheetData struct {
+	Rows []map[string]string //行，每一行的数据以标题为标准key，存储
 }
 
 //getExcelAllCell 使用tealeg解析，时间不完美
@@ -60,12 +58,8 @@ func getExcelRows(excelURL string) error {
 }
 
 //createExcel 新建一个excel
-func (e *Excel) createExcel() {
-	if len(e.SheetDatas) != e.SheetNum {
-		return
-	}
+func (e *excel) createExcel() {
 	file := xlsx.NewFile()
-
 	for i, v := range e.SheetDatas {
 		sheetName := "Sheet" + strconv.Itoa(i)
 		// Create a new sheet.
@@ -90,6 +84,25 @@ func (e *Excel) createExcel() {
 	if err := file.Save(e.FileName + ".xlsx"); err != nil {
 		core.LOG.WithFields(logrus.Fields{"excel": err}).Error("file")
 	}
+}
+
+//CreateExcel 创建一个excel
+//name为文件名，不需要后缀
+//后续参数为每个页面的数据，每一个rowData参数对应一个sheet页面,内部数据就是sheet的数据
+//example: CreateExcel("example",data1,data2)
+//执行后生成文件名称为example.xlsx,内部有两个sheet页，sheet1数据内容为dat1，sheet2数据内容为dat2
+func CreateExcel(name string, rowData ...[]map[string]string) {
+	sheetDatas := []sheetData{}
+	for _, v := range rowData {
+		sd := sheetData{}
+		sd.Rows = v
+		sheetDatas = append(sheetDatas, sd)
+	}
+	el := excel{
+		FileName:   name,
+		SheetDatas: sheetDatas,
+	}
+	el.createExcel()
 }
 
 // Export 导出
