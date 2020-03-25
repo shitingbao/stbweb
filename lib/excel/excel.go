1f65ac80dbf7b2f442648b70f76b08e484c3f47f
shitingbao <871258317@qq.com>
2020-03-25 15:49:55 +0800

    增加excel操作

--- lib/excel/excel.go
+++ lib/excel/excel.go
@@ -3,10 +3,25 @@ package excel
 import (
 	"fmt"
 	"stbweb/core"
+	"strconv"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/Sirupsen/logrus"
 	"github.com/tealeg/xlsx"
 )
+
+//Excel 数据对象
+type Excel struct {
+	FileName   string //文件名称，不需要文件后缀
+	SheetNum   int    //sheet的数量,与sheet的数据数组长度对应
+	SheetDatas []SheetData
+}
+
+//SheetData sheet数据内容
+type SheetData struct {
+	Title []string            //标题列
+	Rows  []map[string]string //行，每一行的数据以标题为标准key，存储
+}
 
 //getExcelAllCell 使用tealeg解析，时间不完美
 func getExcelAllCell(fileURL string) error {
@@ -42,6 +57,39 @@ func getExcelRows(excelURL string) error {
 		}
 	}
 	return nil
+}
+
+//createExcel 新建一个excel
+func (e *Excel) createExcel() {
+	if len(e.SheetDatas) != e.SheetNum {
+		return
+	}
+	file := xlsx.NewFile()
+
+	for i, v := range e.SheetDatas {
+		sheetName := "Sheet" + strconv.Itoa(i)
+		// Create a new sheet.
+		sheet, err := file.AddSheet(sheetName)
+		if err != nil {
+			return
+		}
+		for idv, dv := range v.Rows {
+			row := sheet.AddRow()
+			row.SetHeightCM(1) //设置每行的高度
+			for key, val := range dv {
+				cell := row.AddCell()
+				if idv == 0 {
+					cell.Value = key
+				} else {
+					cell.Value = val
+				}
+			}
+		}
+	}
+
+	if err := file.Save(e.FileName + ".xlsx"); err != nil {
+		core.LOG.WithFields(logrus.Fields{"excel": err}).Error("file")
+	}
 }
 
 // Export 导出
