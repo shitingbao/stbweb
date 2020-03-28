469f4710679b4cb918846e9ddbd686830a17cdc5
shitingbao <871258317@qq.com>
2020-03-28 09:03:25 +0800

    config

--- core/sendjson.go
+++ core/sendjson.go
@@ -28,7 +28,7 @@ func SendJSON(w http.ResponseWriter, statuscode int, data interface{}) {
 
 	w.Header().Set(ContentType, ContentJSON+";"+defaultCharset)
 	if WebConfig.AllowCORS {
-		w.Header().Set("Access-Control-Allow-Origin", "*") //设置允许跨域的请求地址
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080") //设置允许跨域的请求地址
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", fmt.Sprintf(
