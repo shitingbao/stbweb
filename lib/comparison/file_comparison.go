e3290bb7339fb89dc2ec55f9c0820889e1d50452
shitingbao <871258317@qq.com>
2020-05-08 17:42:42 +0800

    增加csv和txt文本比对模块

---
+++ lib/comparison/file_comparison.go
@@ -0,0 +1,4 @@
+package comparison
+
+//FileComparison 是否有标题，分隔符时什么，csv也可以定义分隔符
+func FileComparison() {}
---
+++ lib/comparison/file_help.go
@@ -0,0 +1,60 @@
+//Package comparison 该包适用于txt以及csv文件，excel文件使用excel中的比对
+package comparison
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+//逐行读取的三种方法
+func readLineFile(fileName string) {
+	if file, err := os.Open(fileName); err != nil {
+		panic(err)
+	} else {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			log.Println("NewScanner:", scanner.Text())
+		}
+	}
+}
+
+func readFileLine(fileName string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	buf := bufio.NewReader(f)
+	for {
+		line, err := buf.ReadString('\n')
+		line = strings.TrimSpace(line)
+		log.Println("n:", line)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+	}
+}
+
+func readLine(fileName string) {
+	fi, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer fi.Close()
+
+	br := bufio.NewReader(fi)
+	for {
+		a, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+		log.Println("line:", string(a))
+	}
+}
