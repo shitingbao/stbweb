fdeed520f338945ecf0c8cfdff4b6c5087729714
shitingbao <871258317@qq.com>
2020-05-15 21:48:31 +0800

    增加类型判断

--- lib/game/double_buckle.go
+++ lib/game/double_buckle.go
@@ -27,3 +27,70 @@ func (db *DoubleBuckle) GetBrandType(d DeckOfCards) string {
 	}
 	return ""
 }
+
+//JudgeStraight 判断是顺子
+func JudgeStraight(str []int) bool {
+	if len(str) < 5 {
+		return false
+	}
+	for i := 0; i < len(str)-1; i++ {
+		if str[i+1]-str[i] != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+//JudgeEvenPair 连对判断,只要判断到倒数第二和倒数第一相等，倒数第二和倒数第三差1即可
+func JudgeEvenPair(str []int) bool {
+	if len(str) < 6 && len(str)%2 != 0 {
+		return false
+	}
+	for i := 0; i < len(str)-1; i++ {
+		switch {
+		case i%2 == 0:
+			if str[i] != str[i+1] {
+				return false
+			}
+		default:
+			if str[i+1]-str[i] != 1 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+//JudgeTriplePair 三连对
+func JudgeTriplePair(str []int) bool {
+	if len(str) >= 9 && len(str)%3 != 0 {
+		return false
+	}
+	for i := 0; i < len(str)-2; i++ {
+		switch {
+		case i%3 == 0:
+			if !(str[i] == str[i+1] && str[i] == str[i+2]) {
+				return false
+			}
+		case i%3 == 1:
+			if str[i+2]-str[i] != 1 {
+				return false
+			}
+		default:
+		}
+	}
+	return true
+}
+
+//Judgebomb 炸弹
+func Judgebomb(str []int) bool {
+	if len(str) < 4 {
+		return false
+	}
+	for i := 0; i < len(str)-1; i++ {
+		if str[i] != str[i+1] {
+			return false
+		}
+	}
+	return true
+}
