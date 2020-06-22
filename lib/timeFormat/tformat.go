4b50af087ec9a37ce8df1c1f3cc16c92f00f5462
shitingbao <871258317@qq.com>
2020-06-22 15:50:31 +0800

    Last-Modified

--- lib/timeFormat/tformat.go
+++ lib/timeFormat/tformat.go
@@ -21,3 +21,18 @@ func gmtTotime() {
 func getGMTtime() {
 	fmt.Println(time.Now().UTC().Format(http.TimeFormat))
 }
+
+//获取服务器文件更新时间信息
+//header中的Last-Modified属性就是该文件的更新时间信息，格式是给gmt
+func getServeFileInfo(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	lastModified := resp.Header.Get("Last-Modified")
+	t, err := time.Parse(http.TimeFormat, lastModified)
+	if err != nil {
+		return "", err
+	}
+	return t.Add(time.Hour * 8).Format("2006-01-02 15:04:05"), nil
+}
