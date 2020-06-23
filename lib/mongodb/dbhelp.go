dbf9521c89cbddb7cba87c33172f61e14cb833cd
shitingbao <871258317@qq.com>
2020-06-23 14:43:01 +0800

    mongodb模块增加

--- lib/mongodb/dbhelp.go
+++ lib/mongodb/dbhelp.go
@@ -2,26 +2,49 @@ package mongodb
 
 import (
 	"context"
-	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
+	//这里应用了bson里面的M类型，但是D类型的使用会出错，原因待定
+	//所以所有的条件以及结果集反馈都是用了M类型，其实就是map
 )
 
-//连接一个mongodb，输入连接字符串，待连接数据库，集合名称，反馈该集合对象和error
-func openMongoDb(driver, database, collect string) (*mongo.Collection, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(driver))
+//Mongodb 一个mongo连接对象
+//保存数据库名称（Database），集合名称（Collect），集合对象（CollectionDB用于操作），过期时间（OutTime，默认10S,可以重新指定），Cancelf取消函数（释放资源函数）
+type Mongodb struct {
+	Database     string
+	Collect      string
+	CollectionDB *mongo.Collection
+	OutTime      time.Duration
+	Cancelf      context.CancelFunc
+	Ctx          context.Context
+}
+
+//OpenMongoDb 连接一个mongodb
+//输入连接字符串，待连接数据库，集合名称，反馈该集合对象和error
+func OpenMongoDb(driver, database, collect string) (*Mongodb, error) {
+	ctx, canf := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer canf()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(driver))
 	if err != nil {
 		return nil, err
 	}
 	clo := client.Database(database).Collection(collect)
-	return clo, nil
+	return &Mongodb{
+		Database:     database,
+		Collect:      collect,
+		CollectionDB: clo,
+		OutTime:      10 * time.Second,
+		Cancelf:      canf,
+		Ctx:          ctx,
+	}, nil
 }
 
-//插入一条文档，传入一个content,一个集合对象，和待插入的bson的对象数据，反馈该数据的id和error
-func insertOne(ctx context.Context, clo *mongo.Collection, data bson.M) (interface{}, error) {
-	res, err := clo.InsertOne(ctx, data)
+//InsertOne 插入一条文档，传入一个content,一个集合对象，和待插入的bson的对象数据，反馈该数据的id和error
+func (m *Mongodb) InsertOne(data bson.M) (interface{}, error) {
+	res, err := m.CollectionDB.InsertOne(m.Ctx, data)
 	if err != nil {
 		return nil, err
 	}
@@ -29,9 +52,9 @@ func insertOne(ctx context.Context, clo *mongo.Collection, data bson.M) (interfa
 	return id, nil
 }
 
-//插入多条数据
-func insertMany(ctx context.Context, clo *mongo.Collection, data bson.M) (interface{}, error) {
-	res, err := clo.InsertOne(ctx, data)
+//InsertMany 插入多条数据
+func (m *Mongodb) InsertMany(data bson.M) (interface{}, error) {
+	res, err := m.CollectionDB.InsertOne(m.Ctx, data)
 	if err != nil {
 		return nil, err
 	}
@@ -39,27 +62,27 @@ func insertMany(ctx context.Context, clo *mongo.Collection, data bson.M) (interf
 	return id, nil
 }
 
-//查询一条语句，输入集合，条件（bson.D中包含条件数组）,以及接受的结构所以result得是一个struct的指针，反馈查询出来数据
+//Selectone 查询一条语句，输入集合，条件（bson.D中包含条件数组）,以及接受的结构所以result得是一个struct的指针，反馈查询出来数据
 //opt是限制函数
-func selectone(ctx context.Context, clo *mongo.Collection, where bson.D, opts ...*options.FindOneOptions) (bson.M, error) {
+func (m *Mongodb) Selectone(where bson.M, opts ...*options.FindOneOptions) (bson.M, error) {
 	var result bson.M
-	if err := clo.FindOne(ctx, where, opts...).Decode(&result); err != nil {
+	if err := m.CollectionDB.FindOne(m.Ctx, where, opts...).Decode(&result); err != nil {
 		return nil, err
 	}
 	return result, nil
 }
 
-//查询所有，输入集合和条件（bson.D中包含条件数组），反馈所有数据和error
+//SelectAll 查询所有，输入集合和条件（bson.D中包含条件数组），反馈所有数据和error
 //opt是限制函数
-func selectAll(ctx context.Context, clo *mongo.Collection, where bson.D, opts ...*options.FindOptions) ([]bson.M, error) {
+func (m *Mongodb) SelectAll(where bson.M, opts ...*options.FindOptions) ([]bson.M, error) {
 	var result []bson.M
-	cur, err := clo.Find(ctx, where, opts...)
+	cur, err := m.CollectionDB.Find(m.Ctx, where, opts...)
 	if err != nil {
 		return nil, err
 	}
-	defer cur.Close(ctx)
+	defer cur.Close(m.Ctx)
 
-	for cur.Next(ctx) {
+	for cur.Next(m.Ctx) {
 		var res bson.M
 		if err := cur.Decode(&res); err != nil {
 			return nil, err
@@ -72,34 +95,50 @@ func selectAll(ctx context.Context, clo *mongo.Collection, where bson.D, opts ..
 	return result, nil
 }
 
-//更新一条数据
-func updateOne(ctx context.Context, clo *mongo.Collection, update interface{}, where bson.D, opts ...*options.UpdateOptions) {
-	res, err := clo.UpdateOne(ctx, where, update, opts...)
+//UpdateOne 更新一条数据
+func (m *Mongodb) UpdateOne(update interface{}, where bson.M, opts ...*options.UpdateOptions) (interface{}, error) {
+	res, err := m.CollectionDB.UpdateOne(m.Ctx, where, update, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
-	log.Println(res.UpsertedID)
+	return res.UpsertedID, nil
 }
 
-//更新一条数据
-func updateMany(ctx context.Context, clo *mongo.Collection, update interface{}, where bson.D, opts ...*options.UpdateOptions) {
-	res, err := clo.UpdateMany(ctx, where, update, opts...)
+//UpdateMany 更新一条数据
+func (m *Mongodb) UpdateMany(update interface{}, where bson.M, opts ...*options.UpdateOptions) (interface{}, error) {
+	res, err := m.CollectionDB.UpdateMany(m.Ctx, where, update, opts...)
 	if err != nil {
-		return
+		return nil, err
 	}
-	log.Println(res.UpsertedID)
+	return res.UpsertedID, nil
 }
 
-//删除集合
-func deleteCollecting(ctx context.Context, clo *mongo.Collection) {
-	if err := clo.Drop(ctx); err != nil {
-		return
+//DeleteCollecting 删除集合
+func (m *Mongodb) DeleteCollecting() error {
+	if err := m.CollectionDB.Drop(m.Ctx); err != nil {
+		return err
 	}
+	return nil
 }
 
-//删除文档
-func deleteDocument(ctx context.Context, clo *mongo.Collection, where bson.D, opts ...*options.FindOneAndDeleteOptions) {
-	if res := clo.FindOneAndDelete(ctx, where, opts...); res.Err() != nil {
-		return
+//DeleteDocument 删除文档
+func (m *Mongodb) DeleteDocument(where bson.M, opts ...*options.FindOneAndDeleteOptions) error {
+	if res := m.CollectionDB.FindOneAndDelete(m.Ctx, where, opts...); res.Err() != nil {
+		return res.Err()
 	}
+	return nil
+}
+
+//UpOutTime 更新过期时间
+func (m *Mongodb) UpOutTime(outime time.Duration) {
+	m.Cancelf()
+	ctx, canf := context.WithTimeout(context.Background(), outime)
+	m.Ctx = ctx
+	m.OutTime = outime
+	m.Cancelf = canf
+}
+
+//CloseCtx monggodb释放ctx资源
+func (m *Mongodb) CloseCtx() {
+	m.Cancelf()
 }
