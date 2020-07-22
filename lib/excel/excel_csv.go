3dde8f5257fb9bf8e3857cb6fadd2edb27b69557
shitingbao <871258317@qq.com>
2020-07-22 17:13:15 +0800

    GBK形式读取文本文件

--- lib/excel/excel_csv.go
+++ lib/excel/excel_csv.go
@@ -1,8 +1,14 @@
 package excel
 
 import (
+	"bufio"
 	"encoding/csv"
+	"fmt"
+	"log"
 	"os"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 //LoadCsvCfg 解析csv
@@ -94,4 +100,59 @@ func getRes(recordE [][]string) *map[int]Lod {
 		reMapE[i] = da
 	}
 	return &reMapE
+}
+
+var enc = simplifiedchinese.GBK
+
+//GBK形式读csv文件
+func exampleReadGBK(filename string) {
+	// Read UTF-8 from a GBK encoded file.
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := transform.NewReader(f, enc.NewDecoder())
+
+	// Read converted UTF-8 from `r` as needed.
+	// As an example we'll read line-by-line showing what was read:
+	sc := bufio.NewScanner(r)
+	for sc.Scan() {
+		fmt.Printf("Read line: %s\n", sc.Bytes())
+	}
+	if err = sc.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err = f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+//GBK形式写csv文件
+func exampleWriteGBK(filename string) {
+	// Write UTF-8 to a GBK encoded file.
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := transform.NewWriter(f, enc.NewEncoder())
+
+	// Write UTF-8 to `w` as desired.
+	// As an example we'll write some text from the Wikipedia
+	// GBK page that includes Chinese.
+	_, err = fmt.Fprintln(w,
+		`In 1995, China National Information Technology Standardization
+Technical Committee set down the Chinese Internal Code Specification
+(Chinese: 汉字内码扩展规范（GBK）; pinyin: Hànzì Nèimǎ
+Kuòzhǎn Guīfàn (GBK)), Version 1.0, known as GBK 1.0, which is a
+slight extension of Codepage 936. The newly added 95 characters were not
+found in GB 13000.1-1993, and were provisionally assigned Unicode PUA
+code points.`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ practice/binary_search_tree.go
@@ -0,0 +1,11 @@
+package main
+
+type searchTree struct {
+	Value int
+	Left  *searchTree
+	Right *searchTree
+}
+
+func createSearchTree(num int) []*searchTree {
+	return []*searchTree{}
+}
