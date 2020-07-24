dec8d1e9cb39226ae7c39b1b2d0a3e7bc1b3d00f
shitingbao <871258317@qq.com>
2020-07-24 16:28:26 +0800

    动态规划改良

--- practice/programming.go
+++ practice/programming.go
@@ -1,6 +1,8 @@
 package main
 
-//动态规划基本
+import "log"
+
+//动态规划基本,反馈多少种可能性，输入为一个矩形路线
 func minimumTotal(triangle [][]int) int {
 	for i, v := range triangle {
 		if i == 0 {
@@ -29,4 +31,32 @@ func minimumTotal(triangle [][]int) int {
 		}
 	}
 	return min
+}
+
+//动态规划基本,反馈最短路线，输入为一个矩形路线
+func minPathSum(grid [][]int) int {
+	nums := []int{}
+	for j := 0; j < len(grid[0]); j++ {
+		nums = append(nums, 0)
+	}
+
+	for i, v := range grid {
+		for idx, val := range v {
+			if idx == 0 {
+				nums[0] += val
+				continue
+			}
+			if i == 0 {
+				nums[idx] = val + nums[idx-1]
+				continue
+			}
+			if nums[idx]+val > nums[idx-1]+val {
+				nums[idx] = nums[idx-1] + val
+			} else {
+				nums[idx] = nums[idx] + val
+			}
+		}
+		log.Println(nums)
+	}
+	return nums[len(nums)-1]
 }
